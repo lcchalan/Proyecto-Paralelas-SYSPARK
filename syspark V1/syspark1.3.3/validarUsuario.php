341b2afade040d4f7689b197473ed5685ac64f31
Nelson Pupiales <nelsonpupiales@gmail.com>
2015-12-02 10:18:40 -0500

    Agregacion de parqueaderos activos y inactivos

---
+++ syspark V1/syspark1.3.3/validarUsuario.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(0);
+session_start();
+include('connect_mysql.php');
+
+if ($_POST['CedulaAdmin'] == null || $_POST['ClaveAdmin'] == null)
+{
+    echo '<span>Por favor complete todos los campos.</span>';
+}
+else
+{
+    $CedulaAdmin = $_POST['CedulaAdmin']; 
+    $ClaveAdmin = $_POST['ClaveAdmin'];
+
+    $consulta ='select NombreAdmin, ApellidoAdmin, CedulaAdmin, ClaveAdmin from administrador where CedulaAdmin='.$CedulaAdmin.' and ClaveAdmin="'.$ClaveAdmin.'"';
+    $resultado=$conexion->query($consulta);
+    
+    if ($fila=$resultado->fetchArray())
+    {
+        $_SESSION["CedulaAdmin"] = $fila["CedulaAdmin"];
+        $_SESSION["ApellidoAdmin"] = $fila["ApellidoAdmin"];
+        $_SESSION['NombreAdmin']= $fila["NombreAdmin"];
+        echo '<script>location.href = "index.php"</script>';
+    }
+    else
+    {
+        echo '<span>El usuario y/o clave son incorrectas, vuelva a intentarlo.</span>';
+    }
+}
+$conexion->close();
+?>
