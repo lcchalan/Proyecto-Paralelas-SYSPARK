7cc7b29efe2981ca699d5301382597d9ce94809a
santiago <sgmantilla@utpl.edu.ec>
2015-12-02 04:44:45 +0000

    correccion

--- syspark V1/syspark1.3.2/cerrarsesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo ' <center>Ha terminado la sesion </center>';
-echo '<script>$(document).ready(function(){setTimeout(function(){location.href = "index.php";}, 3000);});</script>';
-?>
--- syspark V1/syspark1.3.2/connect_mysql.php
+++
@@ -1,9 +0,0 @@
-<?php 
- $conexion = new SQlite3('syspark.db') or die('no se conecto');
- if ($conexion) {
- 	
- }else{
- 	echo "no se conecto";
- }
- 
-?>
